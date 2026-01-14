2048f78c432fe3213c78c9ced42d2f66477fd7e3
cgamcs <cesar.garciamcs@proton.me>
2026-01-14 17:50:26 -0600

    validando la respuesta de la busqueda con zod

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,9 +1,10 @@
 import type { StateCreator } from "zustand"
 import { getCategories, getRecipes } from "../services/RecipeService"
-import type { Categories, SearchFilter } from "../types"
+import type { Categories, Drinks, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
   categories: Categories
+  drinks: Drinks
   fetchCategories: () => Promise<void>
   searchRecipes: (searchFilters: SearchFilter) => Promise<void>
 }
@@ -12,13 +13,19 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
   categories: {
     drinks: []
   },
+  drinks: {
+    drinks: []
+  },
   fetchCategories: async () => {
     const categories = await getCategories()
     set({
       categories
     })
   },
-  searchRecipes: async (searchFilters) => {
-    await getRecipes(searchFilters)
+  searchRecipes: async (filters) => {
+    const drinks = await getRecipes(filters)
+    set({
+      drinks
+    })
   }
 })
