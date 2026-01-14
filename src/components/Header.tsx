dc2e479e532f3953f1de5b6e46fc3b2b88a4b243
cgamcs <cesar.garciamcs@proton.me>
2026-01-14 17:28:57 -0600

    trabajando la busqueda del formulario

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState, type ChangeEvent } from "react"
+import { useEffect, useMemo, useState, type ChangeEvent, type FormEvent } from "react"
 import { NavLink , useLocation } from "react-router-dom"
 import { useAppStore } from "../stores/useAppStore"
 
@@ -15,6 +15,7 @@ function Header() {
   const fetchCategories = useAppStore((state) => state.fetchCategories)
   const categories = useAppStore((state) => state.categories)
   const { drinks } = categories
+  const searchRecipes = useAppStore((state) => state.searchRecipes)
 
   useEffect(() => {
     fetchCategories()
@@ -25,6 +26,19 @@ function Header() {
       ...searchFilters,
       [e.target.name] : e.target.value
     })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // TODO: Validar
+    if(Object.values(searchFilters).includes('')) {
+      console.log('Todos los campos son obligatorios')
+      return
+    }
+
+    // Consultar la receta
+    searchRecipes(searchFilters)
   }
 
   return (
@@ -51,6 +65,7 @@ function Header() {
         {isHome && (
           <form
             className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+            onSubmit={handleSubmit}
           >
             <div className="space-y-4">
               <label
