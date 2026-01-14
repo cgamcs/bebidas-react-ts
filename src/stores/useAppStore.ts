a459886fdc9c4a474febfa07303d521bc54a612a
cgamcs <cesar.garciamcs@proton.me>
2026-01-14 17:00:42 -0600

    agregando las categorias al state

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice"
 
-export const useAppStore = create<RecipesSliceType>((...a) => ({ // toma una copia de todos los argumentos
+export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({ // (...a) toma una copia de todos los argumentos
   ...createRecipesSlice(...a)
-}))
+})))
