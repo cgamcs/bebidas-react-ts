350cd546945be6e3a4c8614635ca5eeffff3ada8
cgamcs <cesar.garciamcs@proton.me>
2026-01-14 17:59:16 -0600

    creando el codigo para empezar a mostrar las bebidas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
 import { z } from "zod"
-import { CategoriesAPIResponseSchema, DrinksAPIResponse, SearchFilterSchema } from "../schemas/recipes-schema"
+import { CategoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterSchema } from "../schemas/recipes-schema"
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 
 export type Drinks = z.infer<typeof DrinksAPIResponse>
+
+export type Drink = z.infer<typeof DrinkAPIResponse>
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,11 +1,28 @@
+import { useMemo } from "react"
 import { useAppStore } from "../stores/useAppStore"
+import DrinkCard from "../components/DrinkCard"
 
 function IndexPage() {
-  useAppStore((state) => state.categories)
+  const drinks = useAppStore((state) => state.drinks)
+
+  const hasDrinks = useMemo(() => drinks.drinks.length, [drinks])
 
   return (
     <>
-      <h1>IndexPage</h1>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+
+      {hasDrinks ? (
+        <>
+          {drinks.drinks.map(drink => (
+            <DrinkCard
+              key={drink.idDrink}
+              drink={drink}
+            />
+          ))}
+        </>
+      ) : (
+        <p className="my-10 text-center text-2xl">No hay resultados aún, utiliza el formulario para buscar recetas</p>
+      )}
     </>
   )
 }
