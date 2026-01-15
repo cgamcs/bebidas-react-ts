a5ff1491a027d0051a7eb71c2f095ee07b88bd69
cgamcs <cesar.garciamcs@proton.me>
2026-01-14 18:08:50 -0600

    mostrando las bebidas en el front

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -12,14 +12,14 @@ function IndexPage() {
       <h1 className="text-6xl font-extrabold">Recetas</h1>
 
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 my-10 gap-10">
           {drinks.drinks.map(drink => (
             <DrinkCard
               key={drink.idDrink}
               drink={drink}
             />
           ))}
-        </>
+        </div>
       ) : (
         <p className="my-10 text-center text-2xl">No hay resultados aún, utiliza el formulario para buscar recetas</p>
       )}
