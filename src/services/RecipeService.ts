e9c4009489eb130425f11bf2d3069845a688a090
cgamcs <cesar.garciamcs@proton.me>
2026-01-14 18:42:42 -0600

    obteniendo mas informacion de la bebida seleccionada desde la API

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
-import { CategoriesAPIResponseSchema, DrinksAPIResponse } from "../schemas/recipes-schema"
-import type { SearchFilter } from "../types"
+import { CategoriesAPIResponseSchema, DrinksAPIResponse, RecipeAPIResponseSchema } from "../schemas/recipes-schema"
+import type { Drink, SearchFilter } from "../types"
 
 export async function getCategories() {
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
@@ -17,6 +17,17 @@ export async function getRecipes(filters: SearchFilter) {
   const { data } = await axios(url)
 
   const result = DrinksAPIResponse.safeParse(data)
+  if(result.success) {
+    return result.data
+  }
+}
+
+export async function getRecipeById(id: Drink['idDrink']) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+  const { data } = await axios(url)
+
+  const result = RecipeAPIResponseSchema.safeParse(data.drinks[0])
+
   if(result.success) {
     return result.data
   }
