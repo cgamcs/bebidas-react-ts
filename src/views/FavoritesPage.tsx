8fde3cc3ad5d3f5747ea28a6b76f14d09d6cd4d1
César García <cesar.garciamcs@proton.me>
2026-01-22 21:14:35 +0000

    mostrando las bebidas guardadas en favoritos

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,7 +1,29 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 function FavoritesPage() {
+  const favorites = useAppStore((state) => state.favorites)
+
+  const hasFavorite = useMemo(() => favorites.length, [favorites])
+
   return (
-    <div>FavoritesPage</div>
+    <>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+
+      {hasFavorite ? (
+      <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 my-10 gap-10">
+        {favorites.map(favorite => (
+          <DrinkCard
+            key={favorite.idDrink}
+            drink={favorite}
+          />
+        ))}
+      </div>
+      ) : (
+       <p className="my-10 text-center text-2xl">Aún no hay favoritos</p> 
+      )}
+    </>
   )
 }
 
