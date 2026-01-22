052999e7abe9192496dfce75c2c4223d187d7f21
César García <cesar.garciamcs@proton.me>
2026-01-22 20:09:23 +0000

    funcion que agrega una receta a favoritos al hacer click al boton "Agregar a Favoritos"

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -4,8 +4,20 @@ import type { Recipe } from "../types"
 
 export type FavoritesSliceTpe = {
   favorites: Recipe[]
+  handleClickFavorite: (recipe: Recipe) => void
 }
 
-export const createFavoritesSlice : StateCreator<FavoritesSliceTpe> = () => ({
-  favorites: []
+export const createFavoritesSlice : StateCreator<FavoritesSliceTpe> = (set, get) => ({
+  favorites: [],
+  handleClickFavorite: (recipe) => {
+    if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)) {
+      set((state) => ({
+        favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
+      }))
+    } else {
+      set((state) => ({
+        favorites: [...state.favorites, recipe]
+      }))
+    }
+  }
 })
