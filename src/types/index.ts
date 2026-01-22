a86bfa3cc650b9d6d26e489b2c48c50ae81fb253
César García <cesar.garciamcs@proton.me>
2026-01-22 17:20:51 +0000

    almacenando la bebida seleccionada en el state

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { CategoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterSchema } from "../schemas/recipes-schema"
+import { CategoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, RecipeAPIResponseSchema, SearchFilterSchema } from "../schemas/recipes-schema"
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 
@@ -8,3 +8,5 @@ export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
 
 export type Drink = z.infer<typeof DrinkAPIResponse>
+
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>
